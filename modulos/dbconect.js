f16d197e76eb39858350b45132cace0fff57fb61
BMOP98 <133816035+BMOP98@users.noreply.github.com>
2024-05-13 15:06:05 -0500

    camb

--- modulos/dbconect.js
+++ modulos/dbconect.js
@@ -1,5 +1,5 @@
 const {mongoose} = require('mongoose')
 
 mongoose.connect("mongodb+srv://maximo98:BMOPpineda1@cluster0.gqrlqzi.mongodb.net/")
-    .then(db => console.log('Db is connected'))
+    .then(db => console.log('Conectado a la base de datos'))
     .catch(error => console.log(error))
