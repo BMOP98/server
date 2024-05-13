35aff797dd14b0a653c67eeb68d470c3530f5a08
BMOP98 <133816035+BMOP98@users.noreply.github.com>
2024-05-13 15:31:29 -0500

    cambio

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,10 @@ const path = require('path');
 module.exports = {
   resolve: {
     fallback: {
-      "http": require.resolve("stream-http")
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "net": require.resolve("net-browserify"),
+      "tls": require.resolve("tls-browserify"),
     }
-  },
-  module: {
-    rules: [
-      // Aquí puedes agregar reglas de carga de archivos si es necesario
-    ],
-  },
+  }
 };
