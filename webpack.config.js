f8e81111273e743e6a6d607e735aacffd3155886
BMOP98 <133816035+BMOP98@users.noreply.github.com>
2024-05-14 19:25:13 -0500

    c

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // Directorio de salida para los archivos construidos
     filename: 'bundle.js' // Nombre del archivo de salida
   },
+  target: 'node',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -21,5 +23,10 @@ module.exports = {
       // Aquí puedes agregar más reglas para procesar otros tipos de archivos, como CSS, imágenes, etc.
     ]
   },
+  /*polyfill:{
+    fallback:({
+      "url": require.resolve("url/")
+    })
+  }*/
   // Otras configuraciones de Webpack, como plugins, optimizaciones, etc., pueden ir aquí
 };
