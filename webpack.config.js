c4a56226c0ed8445aca51ccd61ec4174d284131a
BMOP98 <133816035+BMOP98@users.noreply.github.com>
2024-05-14 19:10:29 -0500

    s

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Punto de entrada de tu aplicación
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Directorio de salida para los archivos construidos
+    filename: 'bundle.js' // Nombre del archivo de salida
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Regla para archivos JavaScript
+        exclude: /node_modules/, // Excluir archivos dentro de node_modules
+        use: {
+          loader: 'babel-loader', // Usa Babel para transpilar JavaScript
+          options: {
+            presets: ['@babel/preset-env'] // Utiliza el preset de env para compatibilidad con navegadores
+          }
+        }
+      }
+      // Aquí puedes agregar más reglas para procesar otros tipos de archivos, como CSS, imágenes, etc.
+    ]
+  },
+  // Otras configuraciones de Webpack, como plugins, optimizaciones, etc., pueden ir aquí
+};
